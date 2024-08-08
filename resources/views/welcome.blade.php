3ad990d32d5667e248dc7465c023dc1e8f5eb83d
Muhammad Jaafar <65981875+kwaliyo@users.noreply.github.com>
2024-08-08 14:51:27 +0100

    Update welcome.blade.php tailwindcss added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- Styles -->
     <style>
