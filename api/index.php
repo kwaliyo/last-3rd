b229c67c7fc1d3c6b50d1c343402324c72ae8272
kwaliyo <kwaliyo@gmail.com>
2026-09-25 06:19:07 +0100

    fix: suppress PHP 8.5 deprecation notices and set APP_DEBUG=false for Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
+// Suppress PHP deprecation notices in serverless environment
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
+
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+// Suppress PHP deprecation notices
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 define('LARAVEL_START', microtime(true));
 
 /*
